165f5a01989c28c122c6353f4641af202637f2ae
Nurdaulet <ndauletkhan2004@mail.ru>
2024-06-25 22:02:01 +0500

    First commit

---
+++ LeetCode3000/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeetCode3000
+//
+//  Created by Нурдаулет Даулетхан on 25.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
